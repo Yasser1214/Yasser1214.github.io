f99e5d69a8bc756b95994ccab67b8c5af69cc76f
Yasser <yasser_dahy @hotmail.fr>
2022-04-18 17:13:20 +0200

    Projects added

--- portfolio/projects/MarioSearchBar_html/scripts/script.js
+++ portfolio/projects/MarioSearchBar_html/scripts/script.js
@@ -14,30 +14,49 @@ function nameFinder() {
     a_tag = li_tag[i].getElementsByTagName("a")[0]; // serialize the character's name
     txtContent = a_tag.textContent || a_tag.innerText;
     if (txtContent.toUpperCase().indexOf(txtFiltered) == 0 && input_text.value.trim() != "") { 
-    // if the string of the searchbar match the first part of the searched name it displays the name
+    // If the string of the searchbar match the first part of the searched name it displays the item
       li_tag[i].style.display = "";
     } else {
+    // If the search query does not match anymore the item is hidden
       li_tag[i].style.display = "none";
     }
   }
 
 }
 
+
 function hideShowList() {
 
   var list = document.getElementById("list");
   var elmt = list.getElementsByTagName('li');
 
-  for (i = 0; i < elmt.length; i++) {
-    if (elmt[i].style.display = "none") {
-      elmt[i].style.display = "";
-    } 
-  }
+  /* 
+  There is 3 cases :
+    - <ul/> and <li/> are both hidden : we both display them.
+    - <ul/> is displayed and <li/> is hidden : we both display them.
+    - <ul/> and <li/> are both displayed : we keep displaying <ul/> and hide <li/>.
+  Thanks to this, even if only one of them is hidden, when we click on the button the list appears.
+  */
+  if (list.style.display == "none") {
 
-  if (list.style.display == "") {
-    list.style.display = "none";
+    for (i = 0; i < elmt.length; i++) {
+      if (elmt[i].style.display == "none") {
+        elmt[i].style.display = "";
+        list.style.display = "";
+      }
+    }
+
   } else {
-    list.style.display = "";
-  }
 
+    for (i = 0; i < elmt.length; i++) {
+      if (elmt[i].style.display == "none") {
+        elmt[i].style.display = "";
+        list.style.display = "";
+      } else {
+        elmt[i].style.display = "none";
+      }
+    }
+
+  }
+  
 }
